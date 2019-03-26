5228087c61af32ec3158d8ec73a31c07681d1ee7
SeifAldenFarhan <zenseifalden@gmail.com>
2019-03-26 17:35:17 +0200

    Add TFIDF algo and get data from ES

---
+++ getDataES.py
@@ -0,0 +1,164 @@
+import json
+# import logging
+from pprint import pprint
+
+from elasticsearch import Elasticsearch
+
+# The possible indices in storage:
+indices = ['labels', 'landmarks', 'logos', 'web', 'faces', 'text']
+
+# Disable all non-ERROR level messages.
+# This allows to check the connection without logging traceback.
+# logging.basicConfig(level=logging.ERROR)
+
+es = Elasticsearch()  # Represents the elasticsearch client.
+
+
+# ---------------------
+
+def get_data(index, image_id):
+  '''This function gets an image id and index,
+     and prints the data of the image in the index.'''
+
+  if not es.indices.exists(index=index):
+    print('\nThere is no', index, 'index.\n')
+    return False
+
+  if image_id == '':
+    return False
+
+  data = es.get(index=index, doc_type='doc', id=image_id, ignore=404)
+  if data['found'] is False:
+    print('\nThere is no data in', index, 'index for the given image.\n')
+    return False
+  else:
+    print(json.dumps((data), indent=4))
+    return json.dumps((data), indent=4)
+
+
+# -----------------------
+
+def get_images_by_words(words, start=0):
+  '''This function gets words, and prints all images that contain
+     data with the words, at least in one index.'''
+
+  query = {
+    'query': {
+      'bool': {
+        'should': [
+          {'match_phrase': {'labelAnnotations.description': {"query": words, "slop": 100}}},
+          {'match': {'landmarkAnnotations.description': words}},
+          {'match': {'logoAnnotations.description': words}},
+          {'match': {'textAnnotations.description': words}},
+          {'match': {'webDetection.webEntities.description': words}}
+        ]
+      }
+    }  # , fuzzy
+    # 'stored_fields' : ['Image path']
+  }
+
+  files = []
+
+  res = es.search(index='*', doc_type='doc', body=query, _source=['Image path'], from_=start, size=1, ignore=404)
+  print(res)
+
+  if int(res['hits']['total']) == 0:
+    print('\nThere are no images with the word/s \'', words, '\'.')
+
+  else:
+    print('\nAll images with the word/s \'', words, '\' :')
+    for i in res['hits']['hits']:
+      print(i['_id'])
+      files.append(i['_source']['Image path'] + '/' + i['_id'])
+
+  # Return path of all files, and number of total files that were found.
+  return [files, res['hits']['total']]
+
+
+# --------------------------
+
+def get_num_of_documents(index):
+  """This function prints the munber of images in a specific index."""
+
+  if not es.indices.exists(index=index):
+    print('\nThere is no', index, 'index.\n')
+    return
+
+  num = es.count(index=index, doc_type='doc')
+  num = num['count']
+
+  print('\nNumber of images in', index, 'index:', num, '.\n')
+  return num
+
+# --------------------------
+
+def get_all_documents(index, start=0, size=2):
+  '''This function prints a list of all images in a specific index.'''
+
+  if not es.indices.exists(index=index):
+    print('\nThere is no', index, 'index.\n')
+    return None
+
+  image_ids = []
+  description_list = []
+
+  query = {'query': {'match_all': {}}, 'stored_fields': []}
+
+  res = es.search(index=index, doc_type='doc', body=query, from_=start, size=size)
+  # res1 = es.get_source(index=index, doc_type='doc', id='download.jpg')
+  # print(res1['labelAnnotations']['description'])
+
+  if int(res['hits']['total']) == 0:
+    print('\nThere are no images in', index, 'index.\n')
+
+  else:
+    print('\nAll images in', index, 'index:')
+    for i in res['hits']['hits']:
+      # print(i['_id'])
+      # print(i['_source'])
+      # print(i['_score'])
+      image_ids.append(i['_id'])
+
+      # ---- get the description for each image
+      # res1 = es.get_source(index=index, doc_type='doc', id=i['_id'])
+      # print([i['description'] for i in res1['labelAnnotations']])
+      # description_list.append([i['description'] for i in res1['labelAnnotations']])
+
+  # pprint(description_list)
+  return image_ids
+
+
+# ----------------------
+
+def get_all_descriptions(index, image_ids):
+  description_list = []
+
+  for id in image_ids:
+    res1 = es.get_source(index=index, doc_type='doc', id=id)
+    # print([i['description'] for i in res1['labelAnnotations']])
+    description_list.append([i['description'] for i in res1['labelAnnotations']])
+
+  return description_list
+
+
+def write_to_file(descriptionList):
+  with open("description", "w") as f:
+    for item in descriptionList:
+      for i in item:
+        f.write("%s, " % i)
+      f.write(".\n")
+
+
+# ---------------
+# ------- MAIN
+# ---------------
+
+size = get_num_of_documents('labels')
+documents_ids = get_all_documents('labels', 0, size)
+pprint(documents_ids)
+print()
+print("----------------")
+print()
+descriptions = get_all_descriptions('labels', documents_ids)
+# pprint(descriptions)
+write_to_file(descriptions)
---
+++ tfidf.py
@@ -0,0 +1,161 @@
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import math
+from pprint import pprint
+
+# text = ""
+# with open("description", "r") as f:
+#   for line in f.readlines():
+#     text += line
+#     # text += '.'
+
+
+def get_descripion(file_name):
+  text = ""
+  with open(file_name, "r") as f:
+    for line in f.readlines():
+      text += line
+      # text += '.'
+  return text
+
+
+# -------------------------------------
+def remove_string_special_charactors(s):
+  """
+  This function removes special characors from within a string
+  :parameter :
+    s(str): single input string.
+
+  :return:
+    stripped(str): A string with special characters removed
+  """
+  stripped = re.sub('[^\w\s]', '', s)
+  stripped = re.sub('_', '', stripped)
+  stripped = re.sub('\s+', ' ', stripped)
+  stripped = stripped.strip()
+
+  return stripped
+
+
+# -------------------------------------
+def get_doc(text):
+  """
+  This function splits the text into sentence and
+  considering each sentence as a document, calculates the
+  total word count of each.
+  """
+  doc_info = []
+  i = 0
+  for sent in text:
+    i += 1
+    count = count_words(sent)
+    temp = {'doc_id': i, 'doc_length': count}
+    doc_info.append(temp)
+  return doc_info
+
+
+# -------------------------------------
+def count_words(sent):
+  """
+  This function returs the total
+  number of words in the input text.
+  """
+  count = 0
+  words = word_tokenize(sent)
+  for word in words:
+    count += 1
+  return count
+
+
+# -------------------------------------
+def create_freq_dict(sents):
+  """
+  This function creates a frequency dictionary
+  fir each word in each document.
+  """
+  i = 0
+  freqDict_list = []
+  for sent in sents:
+    i += 1
+    freq_dict = {}
+    words = word_tokenize(sent)
+    # pprint(words)
+    for word in words:
+      word = word.lower()
+      if word in freq_dict:
+        freq_dict[word] += 1
+      else:
+        freq_dict[word] = 1
+      temp = {'doc_id': i, 'freq_dict': freq_dict}
+    freqDict_list.append(temp)
+  return freqDict_list
+
+
+# -------------------------------------
+def computeTF(doc_info, freqDict_list):
+  """
+  tf = (frequency of the term in the doc/total number of term im the doc)
+  """
+  TF_scores = []
+  for tempDict in freqDict_list:
+    id = tempDict['doc_id']
+    for k in tempDict['freq_dict']:
+      temp = {'doc_id': id,
+              'TF_score': tempDict['freq_dict'][k]/doc_info[id-1]['doc_length'],
+              'key': k}
+      TF_scores.append(temp)
+  return TF_scores
+
+
+# -------------------------------------
+def computeIDF(doc_info, freqDict_list):
+  """
+  idf = ln(total number of docs/number of docs with term in it)
+  """
+  IDF_scores = []
+  counter = 0
+  for dict in freqDict_list:
+    counter += 1
+    for k in dict['freq_dict'].keys():
+      count = sum([k in tempDict['freq_dict'] for tempDict in freqDict_list])
+      temp = {'doc_id': counter, 'IDF_score': math.log(len(doc_info)/count), 'key': k}
+      IDF_scores.append(temp)
+  return IDF_scores
+
+
+# -------------------------------------
+def computeTFIDF(TF_scores, IDF_scores):
+  TFIDF_scores = []
+  for j in IDF_scores:
+    for i in TF_scores:
+      if j['key'] == i['key'] and j['doc_id'] == i['doc_id']:
+        temp = {'doc_id': j['doc_id'],
+                'TFIDF_score': (j['IDF_score'] * i['TF_score']),
+                'key': i['key']}
+    # pprint(temp)
+    TFIDF_scores.append(temp)
+  TFIDF_scores = sorted(TFIDF_scores, key=lambda score: score['TFIDF_score'], reverse=False)
+  return TFIDF_scores
+
+
+# -------------------------------------
+# MAIN
+text = get_descripion("description")
+text_sents = sent_tokenize(text)
+text_sents_clean = [remove_string_special_charactors(s) for s in text_sents]
+doc_info = get_doc(text_sents_clean)
+# pprint(doc_info)
+
+freqDict_list = create_freq_dict(text_sents_clean)
+# pprint(freqDict_list)
+
+TF_scores = computeTF(doc_info, freqDict_list)
+IDF_scores = computeIDF(doc_info, freqDict_list)
+TFIDF_score = computeTFIDF(TF_scores, IDF_scores)
+
+# pprint(TF_scores)
+# pprint(IDF_scores)
+pprint(TFIDF_score)
+# pprint(remove_string_special_charactors(text))
